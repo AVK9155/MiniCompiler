59883e95a45d3384305b3a813dbf61dd0f8ff51f
aarvikk <damubalaji144@gmail.com>
2025-12-26 10:42:23 +0530

    Frontend compile integration completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [code, setCode] = useState("");
+  const [output, setOutput] = useState("");
+
+  const compileCode = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/compile", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ code })
+      });
+
+      const data = await response.json();
+      setOutput(
+  "TOKENS:\n" +
+  JSON.stringify(data.tokens, null, 2) +
+  "\n\nSYMBOL TABLE:\n" +
+  JSON.stringify(data.symbolTable, null, 2)
+);
+
+    } catch (error) {
+      console.error(error);
+      setOutput("Error connecting to backend");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>Mini Compiler</h1>
+
+      <textarea
+        rows="10"
+        cols="80"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+        placeholder="Write your C code here..."
+      />
+
+      <br /><br />
+
+      <button onClick={compileCode}>Compile</button>
+
+      <pre>{output}</pre>
     </div>
   );
 }
